858433f12f1d268c6ddf9e4eccc9056d0cf4c79a
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2017-03-20 23:39:49 -0300

    Ajuste na aplicacao para o uso do sqlite e ajustes em erro do template.

--- djangoecommerce/local_settings.py
+++ djangoecommerce/local_settings.py
@@ -7,11 +7,8 @@ DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'e_commerce',
-    'HOST': '127.0.0.1',
-    'PORT': '3306',
-    'USER': 'root',
-    'PASSWORD': '4n70n10',
-}}
+    'default':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
